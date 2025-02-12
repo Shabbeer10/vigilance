644b04c51e1db62ec5d127b2b23146bac763a3eb
Shabbeer Petersen <shabbeer.petersen@capaciti.org.za>
2025-02-12 13:35:50 +0200

    created api routes for logs and alerts

---
+++ server/routes/apiRoutes.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import Log from '../models/Log.js';
+import Alert from '../models/Alerts.js';
+
+const router = express.Router();
+
+// Retrieve all logs
+router.get('/logs', async (req,res) => {
+    try {
+        const logs = await Log.find().sort({timestamp: -1});
+        res.json(logs);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+// retrieve all alerts
+router.get('/alerts', async (req,res) => {
+    try {
+        const alerts = await Alert.find().populate('log').sort({timestamp: -1});
+        res.json(alerts);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+export default router;
